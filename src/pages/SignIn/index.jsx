8c40a968c50f0f584661e8f2a21b1dffdb461a74
Eduardo Augusto <eduardo.afreis@gmail.com>
2024-02-07 17:42:30 -0300

    Contexto criado e redirecionamento das rotas concluído

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+
+import {useAuth} from "../../hooks/auth";
+
 import { Container, Form, Brand } from "./styles"
 
 import {Link} from "react-router-dom";
@@ -10,6 +14,16 @@ import foodExplore from "../../assets/food explorer.png";
 import polygon from "../../assets/Polygon 1.png";
 
 export function SignIn(){
+
+  const {signIn} = useAuth();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSignIn(){
+    signIn({email, password});
+  }
+
   return(
     <Container>
 
@@ -22,14 +36,22 @@ export function SignIn(){
       <Form>
         <h1>Faça login</h1>
         <Section title="Email">
-        <Input placeholder="Exemplo: exemplo@exemplo.com.br" type="text"/>
+        <Input 
+        placeholder="Exemplo: exemplo@exemplo.com.br" 
+        type="text"
+        onChange={e => setEmail(e.target.value)}
+        />
         </Section>
 
         <Section title="Senha">
-        <Input placeholder="No mínimo 6 caracteres" type="password"/>
+        <Input 
+        placeholder="No mínimo 6 caracteres" 
+        type="password"
+        onChange={e => setPassword(e.target.value)}
+        />
         </Section>
 
-        <Button title="Entrar"/>
+        <Button title="Entrar" type="button" onClick={handleSignIn}/>
           
        <Link to="/register">Criar uma conta</Link>
 
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
+
 import { Container, Form, Brand } from "./styles"
 
-import {Link} from "react-router-dom";
+import {api} from "../../services/api";
+
+import {Link, useNavigate} from "react-router-dom";
 
 import { Input } from "../../components/Input"
 import { Button } from "../../components/Button"
@@ -10,6 +14,34 @@ import foodExplore from "../../assets/food explorer.png";
 import polygon from "../../assets/Polygon 1.png";
 
 export function SignUp(){
+
+  const [name, setName] = useState(""); 
+  const [email, setEmail] = useState(""); 
+  const [password, setPassword] = useState(""); 
+
+  const navigate = useNavigate();
+  
+  function handleSignUp(){
+
+    if(!name || !email || !password){
+      return alert("Preencha todos os campos!");
+    }
+
+    api.post("/users", {name, email, password})
+    .then(() =>{
+      alert("Usuário cadastrado com sucesso!");
+      navigate("/");
+    })
+    .catch(error =>{
+      if(error.response){
+        alert(error.response.data.message);
+      }else{
+        alert("Não foi possível cadastrar.");
+      }
+    })
+
+  }
+
   return(
     <Container>
 
@@ -23,18 +55,34 @@ export function SignUp(){
         <h1>Crie sua conta</h1>
 
         <Section title="Seu nome">
-        <Input placeholder="Exemplo: Maria da Silva" type="text"/>
+        <Input 
+        placeholder="Exemplo: Maria da Silva" 
+        type="text"
+        onChange={e => setName(e.target.value)}
+        />
         </Section>
 
         <Section title="Email">
-        <Input placeholder="Exemplo: exemplo@exemplo.com.br" type="text"/>
+        <Input 
+        placeholder="Exemplo: exemplo@exemplo.com.br" 
+        type="text"
+        onChange={e => setEmail(e.target.value)}
+        />
         </Section>
 
         <Section title="Senha">
-        <Input placeholder="No mínimo 6 caracteres" type="password"/>
+        <Input 
+        placeholder="No mínimo 6 caracteres" 
+        type="password"
+        onChange={e => setPassword(e.target.value)}
+        />
         </Section>
 
-        <Button title="Criar conta"/>
+        <Button 
+        title="Criar conta"
+        type="button"
+        onClick={handleSignUp}
+        />
           
        <Link to="/">Já tenho uma conta</Link>
 
